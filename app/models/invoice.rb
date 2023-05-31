b4897893e4390bd7d3e82efa396fd42afd0b41ff
Lauren Golden <llgolden@gmail.com>
2023-05-31 14:58:30 -0600

    Update enum values

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,4 +3,6 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  enum status: {cancelled: 0, completed: 1, in_progress: 2}
 end
