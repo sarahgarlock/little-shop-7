ce268b356b68335516501404e7ee65244ffbf129
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-02 16:34:34 -0600

    remove pry

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,9 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items
 
   def top_customers(merchant)
-    customers = Customer.joins(invoices: :items)
+    Customer.joins(invoices: :items)
     .where(items: { merchant_id: merchant.id })
     .distinct
-    require 'pry'; binding.pry
   end
 end
