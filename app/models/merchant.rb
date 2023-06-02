fe68569a55357d7430886c58349b8f0faef356bb
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-02 16:52:56 -0600

    comment out method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,9 @@
 class Merchant < ApplicationRecord
   has_many :items
 
-  def top_customers(merchant)
-    Customer.joins(invoices: :items)
-    .where(items: { merchant_id: merchant.id })
-    .distinct
-  end
+  # def top_customers(merchant)
+  #   Customer.joins(invoices: :items)
+  #   .where(items: { merchant_id: merchant.id })
+  #   .distinct
+  # end
 end
