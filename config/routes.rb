b9f988c26dbaafed381f29eba5b68d1338848cd2
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-03 12:04:29 -0600

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
   get "/admin/merchants", to: "admin/merchants#index"
   get "/admin/merchants/:merchant_id", to: "admin/merchants#show"
   get "/admin/merchants/:merchant_id/edit", to: "admin/merchants#edit"
-  patch "/admin/merchants", to: "admin/merchants#update"
+  patch "/admin/merchants/:merchant_id", to: "admin/merchants#update"
+  
   get "/admin/invoices", to: "admin/invoices#index"
-
+  
   get "/merchants/:merchant_id/dashboard", to: "merchant/dashboard#index"
 end
