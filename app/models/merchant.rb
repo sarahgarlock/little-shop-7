ce2bf25dc397f8b9213d1e3302c22bf02b5d4c76
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-03 14:08:28 -0600

    Add status column to merchants with default 0 add enum status to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :items
   validates :name, presence: true
+  enum status: {enabled: 0, disabled: 1}
+  
   # def top_customers(merchant)
   #   Customer.joins(invoices: :items)
   #   .where(items: { merchant_id: merchant.id })
