449b52ca8032a0a5da5d04584bc6a518e29d0d99
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-03 15:32:51 -0600

    WIP params are very nested

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -12,19 +12,22 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def update
+    require 'pry'; binding.pry
     merchant = Merchant.find(params[:merchant_id])
-    if merchant.update(merchant_params)
+    if merchant.update(merchant_params) && merchant_params[:merchant_status].blank?
       redirect_to "/admin/merchants/#{merchant.id}"
       flash[:notice] = "Merchant successfully updated."
-    else
+    elsif merchant_params[:merchant_status].blank?
       redirect_to "/admin/merchants/#{merchant.id}/edit"
       flash[:alert] = "Error: #{error_message(application.errors)}"
+    else  
+      redirect_to admin_merchants_path
     end
   end
 
   private 
   
   def merchant_params
-    params.permit(:name)
+    params.permit(:merchant_id, :name, :status)
   end
 end
--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -29,6 +29,7 @@ RSpec.describe "Admin Merchant Index", type: :feature do
   
   it "has buttons to disable or enable each merchant" do 
     visit admin_merchants_path
+    save_and_open_page
 
     within("#merchant-#{@merchant[0].id}") do 
       expect(page).to have_content("enabled")
@@ -38,19 +39,18 @@ RSpec.describe "Admin Merchant Index", type: :feature do
       expect(page).to have_content("#{@merchant[0].status}")
     end
     
-    expect(page).to have_content("Disable #{@merchant[0].name}")
-    expect(page).to have_content("Disable #{@merchant[1].name}")
-    expect(page).to have_content("Disable #{@merchant[2].name}")
-    expect(page).to have_content("Disable #{@merchant[3].name}")
-    expect(page).to have_content("Disable #{@merchant[4].name}")
-    expect(page).to have_content("Disable #{@merchant[5].name}")
-    expect(page).to have_content("Enable #{@merchant[5].name}")
-    expect(page).to have_content("Enable #{@merchant[4].name}")
-    expect(page).to have_content("Enable #{@merchant[3].name}")
-    expect(page).to have_content("Enable #{@merchant[2].name}")
-    expect(page).to have_content("Enable #{@merchant[1].name}")
-    expect(page).to have_content("Enable #{@merchant[0].name}")
-    
+    expect(page).to have_button("Disable #{@merchant[0].name}")
+    expect(page).to have_button("Disable #{@merchant[1].name}")
+    expect(page).to have_button("Disable #{@merchant[2].name}")
+    expect(page).to have_button("Disable #{@merchant[3].name}")
+    expect(page).to have_button("Disable #{@merchant[4].name}")
+    expect(page).to have_button("Disable #{@merchant[5].name}")
+    expect(page).to have_button("Enable #{@merchant[5].name}")
+    expect(page).to have_button("Enable #{@merchant[4].name}")
+    expect(page).to have_button("Enable #{@merchant[3].name}")
+    expect(page).to have_button("Enable #{@merchant[2].name}")
+    expect(page).to have_button("Enable #{@merchant[1].name}")
+    expect(page).to have_button("Enable #{@merchant[0].name}")
     click_button "Disable #{@merchant[0].name}"
     
     expect(current_path).to eq(admin_merchants_path)
