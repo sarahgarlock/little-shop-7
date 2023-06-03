616d0cf23dc7b7f4037a84668d711253b765a821
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-06-03 15:04:14 -0600

    Feat: delete pry from merchant items controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -2,6 +2,5 @@ class Merchant::ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @items = @merchant.items
-    # require 'pry'; binding.pry
   end
 end
