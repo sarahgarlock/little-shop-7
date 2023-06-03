da9df4dbffb2ed09616fc77e00173c475aaba34e
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-03 11:23:42 -0600

    WIP merchant update

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
 
   get "/admin", to: "admin/dashboard#index"
   get "/admin/merchants", to: "admin/merchants#index"
+  get "/admin/merchants/:merchant_id", to: "admin/merchants#show"
+  get "/admin/merchants/:merchant_id", to: "admin/merchants#edit"
+  patch "/admin/merchants", to: "admin/merchants#update"
   get "/admin/invoices", to: "admin/invoices#index"
 
   get "/merchants/:merchant_id/dashboard", to: "merchant/dashboard#index"
