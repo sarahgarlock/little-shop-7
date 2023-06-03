87c5e2bbed7e88fcb49bce838869449f6641d29d
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-03 11:42:22 -0600

    add validation

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,6 @@
 class Merchant < ApplicationRecord
   has_many :items
-
+  validates :name, presence: true
   # def top_customers(merchant)
   #   Customer.joins(invoices: :items)
   #   .where(items: { merchant_id: merchant.id })
