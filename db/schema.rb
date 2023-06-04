653586af782b2fe4827e056d0326b2cfa766147b
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-03 21:50:21 -0600

    change default merchant status with new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_194534) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_04_034740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,7 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_194534) do
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
-    t.integer "status", default: 0
+    t.integer "status", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
