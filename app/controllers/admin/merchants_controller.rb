871dfa51cbf677727298866e26a7386ba1600792
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-03 22:04:10 -0600

    add new and create methods

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -25,6 +25,14 @@ class Admin::MerchantsController < ApplicationController
     end
   end
 
+  def new
+  end
+
+  def create
+    Merchant.create!(merchant_params)
+    redirect_to admin_merchants_path
+  end
+
   private 
   
   def merchant_params
