ac6ef52e925e6893e85678636dcb08c549331235
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-04 19:58:12 -0600

    add show page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -2,16 +2,15 @@ require "rails_helper"
 
 RSpec.describe "Admin Invoice Show Page", type: :feature do 
   before(:each) do 
-    @customer = create(:customer)
-    @invoice = @customer.create(:invoice)
+    @invoice = create(:invoice)
   end
 
   it "displays all invoice attributes along with customer first and last name" do 
-    visit admin_invoices_path(@invoice)
-save_and_open_page 
+    visit "/admin/invoices/#{@invoice.id}"
+ 
     expect(page).to have_content("ID: #{@invoice.id}")
     expect(page).to have_content("Status: #{@invoice.status}")
     expect(page).to have_content("Created at: #{@invoice.created_at.strftime("%A, %B %d, %Y")}")
-    expect(page).to have_content("Customer: #{@customer.first_name}, #{@customer.last_name}")
+    expect(page).to have_content("Customer: #{@invoice.customer.first_name}, #{@invoice.customer.last_name}")
   end
 end
