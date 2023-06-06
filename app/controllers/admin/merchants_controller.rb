fffe4265f714d349ab05d94bfda0241b07bb05c1
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-06 14:33:07 -0600

    add invalid data flash test for admin merchant

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -20,7 +20,7 @@ class Admin::MerchantsController < ApplicationController
       flash[:notice] = "Merchant successfully updated."
     elsif params[:merchant_status].blank?
       redirect_to "/admin/merchants/#{merchant.id}/edit"
-      flash[:alert] = "Error: #{error_message(application.errors)}"
+      flash[:alert] = "Error: #{error_message(merchant.errors)}"
     else 
       redirect_to admin_merchants_path
     end
