b7076824fb39f2df057a7eecd2c2fe4f2083eda8
Sarah Garlock <garlock.sarah10@gmail.com>
2023-06-06 16:04:57 -0600

    add testing for top by revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,23 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  before :each do
-    @merchant1 = Merchant.create!(name: "Pen Inc.", status: 1)
-    @item1 = Item.create!(name: "pen ink", description: "xxxxxxx", unit_price: 10, merchant_id: @merchant1.id)
-    @item2 = Item.create!(name: "printer ink", description: "xxxxxxx", unit_price: 11, merchant_id: @merchant1.id)
-    @item3 = Item.create!(name: "pens", description: "xxxxxxx", unit_price: 12, merchant_id: @merchant1.id)
-    @customer1 = Customer.create!(first_name: "Andy", last_name: "S")
-    @invoice1 = Invoice.create!(customer_id: @customer1.id, status: 1, created_at: "2023-06-03 03:49:12.81835")
-    @invoice2 = Invoice.create!(customer_id: @customer1.id, status: 1, created_at: "2023-06-02 03:49:12.81835")
-    @invoice3 = Invoice.create!(customer_id: @customer1.id, status: 1, created_at: "2023-06-06 03:49:12.81835")
-    @transaction1 = @invoice1.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
-    @transaction2 = @invoice2.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
-    @transaction3 = @invoice3.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
-    @invoiceitem1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 100, unit_price: 10, status: 1)
-    @invoiceitem2 = InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice2.id, quantity: 100, unit_price: 10, status: 1)
-    @invoiceitem3 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice3.id, quantity: 100, unit_price: 10, status: 1)
-  end
-
   describe 'relationships' do
     it { should have_many :items }
     it { should have_many(:invoice_items).through(:items)}
@@ -30,38 +13,82 @@ RSpec.describe Merchant, type: :model do
   end
   
   describe 'instance methods' do
-    xit 'items_ready_to_ship' do
-      expect(@merchant1.items_ready_to_ship).to eq([@item1, @item2, @item3])
+    it 'items_ready_to_ship' do
+      merchant1 = Merchant.create!(name: "Pen Inc.", status: 1)
+      item1 = Item.create!(name: "pen ink", description: "xxxxxxx", unit_price: 10, merchant_id: merchant1.id)
+      item2 = Item.create!(name: "printer ink", description: "xxxxxxx", unit_price: 11, merchant_id: merchant1.id)
+      item3 = Item.create!(name: "pens", description: "xxxxxxx", unit_price: 12, merchant_id: merchant1.id)
+      customer1 = Customer.create!(first_name: "Andy", last_name: "S")
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 1, created_at: "2023-06-03 03:49:12.81835")
+      invoice2 = Invoice.create!(customer_id: customer1.id, status: 1, created_at: "2023-06-02 03:49:12.81835")
+      invoice3 = Invoice.create!(customer_id: customer1.id, status: 1, created_at: "2023-06-06 03:49:12.81835")
+      transaction1 = invoice1.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
+      transaction2 = invoice2.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
+      transaction3 = invoice3.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
+      invoiceitem1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem2 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice2.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem3 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice3.id, quantity: 100, unit_price: 10, status: 1)
+
+      expect(merchant1.items_ready_to_ship).to eq([item1, item2, item3])
     end
 
-    xit 'update_status' do 
-      expect(@merchant1.status).to eq("disabled")
+    it 'update_status' do 
+      merchant1 = Merchant.create!(name: "Pen Inc.", status: 1)
+      item1 = Item.create!(name: "pen ink", description: "xxxxxxx", unit_price: 10, merchant_id: merchant1.id)
+      item2 = Item.create!(name: "printer ink", description: "xxxxxxx", unit_price: 11, merchant_id: merchant1.id)
+      item3 = Item.create!(name: "pens", description: "xxxxxxx", unit_price: 12, merchant_id: merchant1.id)
+      customer1 = Customer.create!(first_name: "Andy", last_name: "S")
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 1, created_at: "2023-06-03 03:49:12.81835")
+      invoice2 = Invoice.create!(customer_id: customer1.id, status: 1, created_at: "2023-06-02 03:49:12.81835")
+      invoice3 = Invoice.create!(customer_id: customer1.id, status: 1, created_at: "2023-06-06 03:49:12.81835")
+      transaction1 = invoice1.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
+      transaction2 = invoice2.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
+      transaction3 = invoice3.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
+      invoiceitem1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem2 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice2.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem3 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice3.id, quantity: 100, unit_price: 10, status: 1)
 
-      @merchant1.update_status("0")
+      expect(merchant1.status).to eq("disabled")
 
-      expect(@merchant1.status).to eq("enabled")
+      merchant1.update_status("0")
 
-      @merchant1.update_status("1")
+      expect(merchant1.status).to eq("enabled")
 
-      expect(@merchant1.status).to eq("disabled")
+      merchant1.update_status("1")
 
-      expect(@merchant1.update_status("foo")).to eq("invalid code")
+      expect(merchant1.status).to eq("disabled")
+
+      expect(merchant1.update_status("foo")).to eq("invalid code")
     end
 
-    xit 'best_day' do 
-      invoiceitem4 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice3.id, quantity: 100, unit_price: 10, status: 1)
-      invoiceitem5 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice2.id, quantity: 100, unit_price: 10, status: 1)
-      invoiceitem6 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice1.id, quantity: 100, unit_price: 10, status: 1)
-      invoiceitem7 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice1.id, quantity: 100, unit_price: 10, status: 1)
-      invoiceitem8 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice3.id, quantity: 100, unit_price: 10, status: 1)
-      invoiceitem9 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice3.id, quantity: 100, unit_price: 10, status: 1)
-      @invoice1.reload
-      @invoice2.reload
-      @invoice3.reload
-      @merchant2 = create(:merchant)
+    it 'best_day' do 
+      merchant1 = Merchant.create!(name: "Pen Inc.", status: 1)
+      item1 = Item.create!(name: "pen ink", description: "xxxxxxx", unit_price: 10, merchant_id: merchant1.id)
+      item2 = Item.create!(name: "printer ink", description: "xxxxxxx", unit_price: 11, merchant_id: merchant1.id)
+      item3 = Item.create!(name: "pens", description: "xxxxxxx", unit_price: 12, merchant_id: merchant1.id)
+      customer1 = Customer.create!(first_name: "Andy", last_name: "S")
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 1, created_at: "2023-06-03 03:49:12.81835")
+      invoice2 = Invoice.create!(customer_id: customer1.id, status: 1, created_at: "2023-06-02 03:49:12.81835")
+      invoice3 = Invoice.create!(customer_id: customer1.id, status: 1, created_at: "2023-06-06 03:49:12.81835")
+      transaction1 = invoice1.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
+      transaction2 = invoice2.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
+      transaction3 = invoice3.transactions.create!(cc_num: 1234567899876543, cc_exp: 9999999, result: 1)
+      invoiceitem1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem2 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice2.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem3 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice3.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem4 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice3.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem5 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice2.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem6 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice1.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem7 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice1.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem8 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice3.id, quantity: 100, unit_price: 10, status: 1)
+      invoiceitem9 = InvoiceItem.create!(item_id: item3.id, invoice_id: invoice3.id, quantity: 100, unit_price: 10, status: 1)
+      invoice1.reload
+      invoice2.reload
+      invoice3.reload
+      merchant2 = create(:merchant)
 
-      expect(@merchant1.best_day.to_s).to eq(@invoice3.created_at.strftime("%Y-%m-%d"))
-      expect(@merchant2.best_day).to be nil
+      expect(merchant1.best_day.to_s).to eq(invoice3.created_at.strftime("%Y-%m-%d"))
+      expect(merchant2.best_day).to be nil
     end
 
     describe '.top_by_revenue' do
@@ -82,9 +109,9 @@ RSpec.describe Merchant, type: :model do
         invoice_1 = Invoice.create!(customer_id: customer_1.id, status: 1, created_at: "2023-06-03 03:49:12.81835")
         invoice_2 = Invoice.create!(customer_id: customer_1.id, status: 1, created_at: "2023-06-02 03:49:12.81835")
         invoice_3 = Invoice.create!(customer_id: customer_1.id, status: 1, created_at: "2023-06-06 03:49:12.81835")
-        transaction_1 = invoice_1.transactions.create!(result: 1)
-        transaction_2 = invoice_2.transactions.create!(result: 1)
-        transaction_3 = invoice_3.transactions.create!(result: 1)
+        transaction_1 = invoice_1.transactions.create!(result: 1, cc_num: 1234567899876543, cc_exp: 9999999)
+        transaction_2 = invoice_2.transactions.create!(result: 1, cc_num: 1234567899876543, cc_exp: 9999999)
+        transaction_3 = invoice_3.transactions.create!(result: 1, cc_num: 1234567899876543, cc_exp: 9999999)
         invoiceitem_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 30, unit_price: 10, status: 1)
         invoiceitem_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_2.id, quantity: 100, unit_price: 10, status: 1)
         invoiceitem_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_3.id, quantity: 50, unit_price: 10, status: 1)
