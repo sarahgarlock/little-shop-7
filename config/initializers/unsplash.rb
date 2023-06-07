493bdd75ffb89b923a8b8d328c4dd68d16f931be
Sarah Garlock <garlock.sarah10@gmail.com>
2023-06-06 20:32:43 -0600

    add unsplash.configure

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,7 @@
+require 'unsplash'
+
+Unsplash.configure do |config|
+  config.application_access_key = "cUm42o39B75P9EdjCvymqWA1cMCDO3v1Q8r65xCz5dQ"
+  config.application_secret = "Yr3po0PyiqDk_qCQSZ8inKoW1ZMZ4yf_RyKtCImsmpY"
+  config.utm_source = 'little_esty_shop' # Optional, to track usage
+end
